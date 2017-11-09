a847eed49b11238ab8982238c81454dccc75ec6d
Pantelis Giazitsis <giazitsispantelis@gmail.com>
2017-11-10 00:42:21 +0200

    gg

--- main/main.go
+++ main/main.go
@@ -1,30 +1,32 @@
 package main
 
 import (
-  "fmt"
-  "coin_currency/managers"
-  "log"
-  "net/http"
+	"coin_currency/managers"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-  	fmt.Println("Retrieving from ", manager.Shared().Url)
+	fmt.Println("Retrieving from ", manager.Shared().Url)
 
-  	manager.Shared().GetJson()
+	manager.Shared().GetJsonAsync(func() {
+		fmt.Println("Closure called")
+	})
 
-  	http.HandleFunc("/", sayhelloName) // set router
-    err := http.ListenAndServe(":9090", nil) // set listen port
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	fmt.Println("Waitting for closure")
+
+	// http.HandleFunc("/", sayhelloName) // set router
+	// err := http.ListenAndServe(":9090", nil) // set listen port
+	// if err != nil {
+	//     log.Fatal("ListenAndServe: ", err)
+	// }
 
 }
 
-
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()  // parse arguments, you have to call this by yourself
+	r.ParseForm() // parse arguments, you have to call this by yourself
 
-    // Serve JSON
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(manager.Shared().JsonData())
+	// Serve JSON
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(manager.Shared().JsonData())
 }
--- managers/manager.go
+++ managers/manager.go
@@ -1,17 +1,17 @@
 package manager
 
 import (
-	"sync"
-	"net/http"
-	"fmt"
-	"io/ioutil"
 	"coin_currency/models"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
 )
 
 type manager struct {
-	Url string
-	Ids []string
+	Url   string
+	Ids   []string
 	Coins []models.Coin
 }
 
@@ -21,8 +21,8 @@ var sharedManager *manager
 var once sync.Once
 
 func Shared() *manager {
-	once.Do(func () {
-		sharedManager = &manager{Url : "https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=10"}
+	once.Do(func() {
+		sharedManager = &manager{Url: "https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=10"}
 	})
 
 	return sharedManager
@@ -31,25 +31,38 @@ func Shared() *manager {
 func (m *manager) GetJson() {
 	resp, err := http.Get(m.Url)
 	if err != nil {
-		fmt.Println(err)	
+		fmt.Println(err)
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
-	
-	json.Unmarshal(body,&m.Coins)
+
+	json.Unmarshal(body, &m.Coins)
 	fmt.Println("get:\n", m.Coins[0])
-  	
-  	for _, v := range m.Coins {
-  		m.Ids = append(m.Ids, v.Id) 		
-  	} 	
-  	
-  	fmt.Println("All ids:\n", m.Ids)
 
-  	defer resp.Body.Close()
-  }
+	for _, v := range m.Coins {
+		m.Ids = append(m.Ids, v.Id)
+	}
 
-  func (m *manager) JsonData()(d []byte) {
-  	data, _ := json.Marshal(m.Coins)
+	fmt.Println("All ids:\n", m.Ids)
 
-  	return data
-  }
+	defer resp.Body.Close()
+}
+
+func (m *manager) GetJsonAsync(completion func()) {
+	// channel := make(chan bool)
+	go func() {
+		m.GetJson()
+		// channel <- true
+		completion()
+	}()
+
+	// finshed := <-channel
+	// fmt.Println("chanel finished", finshed)
+	// completion()
+}
+
+func (m *manager) JsonData() (d []byte) {
+	data, _ := json.Marshal(m.Coins)
+
+	return data
+}
